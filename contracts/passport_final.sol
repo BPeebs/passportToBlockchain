db66b7fadac834378deb4cc7e612822ccad4b16f
Austin Caras <caras.austin@gmail.com>
2023-04-06 20:16:29 -0700

    add comments to contract

--- contracts/passport_final.sol
+++ contracts/passport_final.sol
@@ -1,5 +1,5 @@
 pragma solidity ^0.8.0;
-
+// create the data structure to hold the travel record info
 contract TravelLog {
     struct TravelRecord {
         string passportID;
@@ -12,20 +12,20 @@ contract TravelLog {
         uint plannedExitDate;
         uint actualExitDate;
     }
-
+// specify the mapping
     mapping (bytes32 => TravelRecord) private travelRecords;
     mapping (string => bool) private passportExists;
     address private admin;
-
+// specify that the admin is the message sender
     constructor() {
         admin = msg.sender;
     }
-
+// require admin-only permission
     modifier onlyAdmin() {
         require(msg.sender == admin, "Only admin can perform this action");
         _;
     }
-
+// function to add initial information on record; include logic requirements
     function addPassportID(
         string memory _passportID,
         uint _passportExpirationDate,
@@ -33,7 +33,7 @@ contract TravelLog {
         string memory _countryOfResidence
     ) public onlyAdmin {
         bytes32 recordKey = keccak256(abi.encodePacked(_passportID));
-        require(!passportExists[_passportID], "Passport record already exists");
+        require(!passportExists[_passportID], "Passport record already exists");  
         require(_passportExpirationDate > block.timestamp + 180 days, "Passport expiration date must be at least 6 months from entry date");
         travelRecords[recordKey] = TravelRecord(
             _passportID,
@@ -48,7 +48,7 @@ contract TravelLog {
         );
         passportExists[_passportID] = true;
     }
-
+// add function to return Home, specifically different from addEntry in that it checks residency and doesnt ask for projected exit date
     function returnHome(string memory _passportID, string memory _countryOfOrigin, string memory _countryOfResidence) public onlyAdmin {
         bytes32 recordKey = keccak256(abi.encodePacked(_passportID));
         TravelRecord storage record = travelRecords[recordKey];
@@ -62,7 +62,7 @@ contract TravelLog {
         record.destinationCountry = '';
     }
 
-
+// add function to add an Entry date
     function addEntryDate(string memory _passportID, uint _plannedExitDate, string memory _countryOfOrigin) public onlyAdmin {
         bytes32 recordKey = keccak256(abi.encodePacked(_passportID));
         TravelRecord storage record = travelRecords[recordKey];
@@ -73,7 +73,7 @@ contract TravelLog {
         record.actualExitDate = 0;
         record.countryOfOrigin = _countryOfOrigin;
     }
-
+// add function to add an exit date
     function addExitDate(string memory _passportID, string memory _destinationCountry) public onlyAdmin {
         bytes32 recordKey = keccak256(abi.encodePacked(_passportID));
         TravelRecord storage record = travelRecords[recordKey];
@@ -84,7 +84,7 @@ contract TravelLog {
         record.countryOfOrigin = record.destinationCountry;
         record.destinationCountry = _destinationCountry;
     }
-
+// add function to query a travel record
     function getTravelRecord(string memory _passportID) public view returns (string memory, uint, string memory, string memory, string memory, string memory, uint, uint, uint) {
         bytes32 recordKey = keccak256(abi.encodePacked(_passportID));
         TravelRecord storage record = travelRecords[recordKey];
